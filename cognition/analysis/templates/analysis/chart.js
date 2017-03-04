96d78653909f3e5215f1f0fdc813c167cde58f8c
Tallis Jeng <alog1024@gmail.com>
2017-03-04 20:48:18 +0800

    time formatted

--- cognition/analysis/templates/analysis/chart.js
+++ cognition/analysis/templates/analysis/chart.js
@@ -3,16 +3,30 @@
 $(function () {
 
     var records = [];
+    var chart;
+    var startTime;
+    var endTime;
 
+    initChart();
+
+    // user click view
     $("#submitTime").click(function () {
+        var date = $("#date").val();
+        var hour1 = $("#hr1").val();
+        var min1 = $("#min1").val();
+        var hour2 = $("#hr2").val();
+        var min2 = $("#min2").val();
+        startTime = date + "T" + hour1 + ":" + min1 + ":00";
+        endTime = date + "T" + hour2 + ":" + min2 + ":00";
         //getChartData();
         drawChart();
     });
 
+
     function getChartData(startTime, endTime) {
 
         $.ajax({
-            url: "/viewHistory",
+            url: "",
             type: "POST",
             dataType: "json",
             contentType: "application/json; charset=utf-8",
@@ -28,54 +42,82 @@ $(function () {
 
     }
 
+    // initial chart
+    function initChart() {
+
+        var test = Math.round(new Date(startTime).getTime() / 1000);
+        console.log(test);
+        var chartData = [
+
+            {
+                label: "Confusion",
+                values: [{
+                    time: Math.round(new Date().getTime() / 1000),
+                    y: 1020
+                }, {
+                    time: Math.round(new Date().getTime() / 1000),
+                    y: 1080
+                }, {
+                    time: Math.round(new Date().getTime() / 1000),
+                    y: 500
+                }, {
+                    time: Math.round(new Date().getTime() / 1000),
+                    y: 140
+                }, {
+                    time: Math.round(new Date().getTime() / 1000),
+                    y: 340
+                }]
+            }
+        ];
+
+        chart = $('#chart').epoch({
+            type: 'time.area',
+            data: chartData,
+            axes: ['top', 'right', 'bottom', 'left'],
+            ticks: {
+                time: 10,
+                right: 5,
+                left: 5
+            },
+            tickFormats: {
+                time: function (d) {
+                    return new Date(time * 1000).toString();
+                }
+            }
+
+        });
+    }
+
+
     function drawChart() {
         snackbarMessage("draw!");
+
+        // format data
         var dtPoints = [];
         for (var i = 0; i < records.length; i++) {
             var point = {
-                x: records[i][0],
+                time: records[i][0],
                 y: records[i][1]
             };
             dtPoints.push(point);
         }
 
-        dtPoints = [{
-            x: 0,
-            y: 100
-        }, {
-            x: 20,
-            y: 1000
-        }];
-
-
-
-        var lineChartData = [
-
-            {
-                label: "Confusion",
-                values: dtPoints
-            },
-            {
-                label: "Satisfaction",
-                values: [{
-                    x: 20,
-                    y: 1000
-                }, {
-                    x: 30,
-                    y: 9800
-                }]
-            }
-        ]
-        console.log(lineChartData[0].values[0]);
-
-        // chart
-        var chart = $('#chart').epoch({
-            type: 'area',
-            data: lineChartData
+        dtPoints.push({
+            time: new Date().getMilliseconds(),
+            y: 200
         });
+
+        // plot the chart
+        var delayMillis = 1000; //1 second
+
+        for (var j = 0; j < dtPoints.length; j++) {
+            setTimeout(function () {
+                $('#chart').push(dtPoints[j]);
+                //$('#chart').redraw();
+            }, delayMillis);
+        }
+
     }
-
-
 
     //snackbar
     var snackbar = document.getElementById("snackbar");
@@ -89,8 +131,5 @@ $(function () {
             snackbar.className = snackbar.className.replace("show", "");
         }, 2500);
     }
-
-
-
 
 });
